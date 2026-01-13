5ecd1ea4ceb46092d593a1afc022b437f896da44
wsadd <126190446+wsadd@users.noreply.github.com>
2026-01-13 20:01:29 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,          
-    allowedHosts: "all",  
+    host: true,
+    allowedHosts: "all",
+    hmr: {
+      host: "projektinz2026.pl",
+    },
   },
 });
