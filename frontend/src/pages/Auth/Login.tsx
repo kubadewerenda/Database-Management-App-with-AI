74adf4d2c0933549829fb3c9e95563db3e0857f5
Marek <Marek@DESKTOP-2G6B3Q4>
2026-01-17 13:44:20 +0100

    clean

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
       await login(email, password);
       navigate("/dashboard");
     } catch (error) {
-      console.error("Error logowanie", error);
+      console.error("Error logowania", error);
     }
   };
 
